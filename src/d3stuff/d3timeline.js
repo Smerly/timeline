7c9dded60f8b5ef90eaa765137fccab84e33e414
Ernest Cheung <ernestcheung02@gmail.com>
2023-07-24 23:28:33 -0700

    fixed timeline

--- src/d3stuff/d3timeline.js
+++ src/d3stuff/d3timeline.js
@@ -2,23 +2,62 @@ import { useRef, useEffect } from 'react';
 import { useResize } from '../resize.js';
 import { select } from 'd3-selection';
 import timelineDrawing from '../drawings/timelineDrawing';
+import { drawTimelineEvents } from '../drawings/drawTimelineEvents';
 
 function D3timeline() {
+	console.log(new Date('2023, 01, 01').getTime());
+
 	const data = [
 		{
-			color: 'red',
-			ts: Date.now(),
-			title: 'hi',
+			color: 'yellow',
+			ts: 1659769200000,
+			title: 'August 6th 2022',
+		},
+		{
+			color: 'green',
+			ts: 1660978800000,
+			title: 'August 20th 2022',
+		},
+		{
+			color: 'orange',
+			ts: 1660978800000,
+			title: 'October 20th, 2022',
+		},
+		{
+			color: 'orange',
+			ts: 1660978800000,
+			title: 'October 20th, 2022',
+		},
+		{
+			color: 'orange',
+			ts: 1660978800000,
+			title: 'October 20th, 2022',
+		},
+		{
+			color: 'orange',
+			ts: 1660978800000,
+			title: 'October 20th, 2022',
+		},
+		{
+			color: 'orange',
+			ts: 1660978800000,
+			title: 'October 20th, 2022',
+		},
+
+		{
+			color: 'green',
+			ts: 1682924400000,
+			title: 'May 1st 2023',
 		},
 		{
 			color: 'yellow',
-			ts: 1690128079017,
-			title: 'lol',
+			ts: 1684566000000,
+			title: 'May 20th 2023',
 		},
 		{
-			color: 'blue',
-			ts: 1690145779087,
-			title: 'yo',
+			color: 'red',
+			ts: 1691305200000,
+			title: 'Aug 6 2023',
 		},
 	];
 	// Getting the size of the root Div element (being the screen)
@@ -34,11 +73,12 @@ function D3timeline() {
 				.attr('width', size.width)
 				.attr('height', size.height);
 			timelineDrawing(rootRef.current, size, data);
+			drawTimelineEvents(rootRef.current, size, data);
 		}
 	}, [size]);
 
 	return (
-		<div ref={rootRef} id="d3-timeline-widget">
+		<div ref={rootRef} style={{ height: 500 }}>
 			d3timeline
 		</div>
 	);
--- src/drawings/drawTimeline.js
+++ src/drawings/drawTimeline.js
@@ -1,6 +1,73 @@
+// import { select } from 'd3-selection';
+// import { axisBottom } from 'd3-axis';
+// import { scaleLinear } from 'd3-scale';
+// import { drawTimelineEvents } from './drawTimelineEvents';
+// import { timeMonth } from 'd3-time';
+// import * as d3 from 'd3';
+
+// export const drawTimeline = (rootElement, windowSize, timelineEvents) => {
+// 	const { width, height } = windowSize;
+
+// 	const start = new Date('2022, 08, 06');
+// 	const end = new Date('2023, 08, 06');
+// 	// console.log(timelineEvents[0].ts);
+// 	// console.log(timelineEvents[timelineEvents.length - 1].ts);
+// 	const PADDING = width / 10;
+// 	const scale = scaleLinear()
+// 		.domain([
+// 			timelineEvents[0].ts,
+// 			timelineEvents[timelineEvents.length - 1].ts,
+// 		])
+// 		.range([0 + PADDING, width - PADDING])
+// 		.nice();
+// 	const axis = axisBottom(scale)
+// 		.ticks()
+// 		.tickFormat((d, i) => {
+// 			console.log('d', d);
+// 			console.log('i', i);
+
+// 			const date = new Date(timelineEvents[i].ts);
+// 			// const date = new Date(d);
+
+// 			console.log('timeo', date.getTime());
+// 			return `${date.toLocaleDateString()}`;
+// 		});
+// 	// .tickFormat(d3.timeFormat('%b'));
+// 	// .tickValues([
+// 	// 	'Jan',
+// 	// 	'Feb',
+// 	// 	'Mar',
+// 	// 	'Apr',
+// 	// 	'May',
+// 	// 	'Jun',
+// 	// 	'Jul',
+// 	// 	'Aug',
+// 	// 	'Sep',
+// 	// 	'Oct',
+// 	// 	'Nov',
+// 	// 	'Dec',
+// 	// ]);
+
+// 	select(rootElement)
+// 		.select('svg')
+// 		.append('g')
+// 		.attr('class', 'x-axis')
+// 		.attr('width', width)
+// 		.attr('transform', `translate(${0}, ${height * (1 / 2)})`)
+// 		.call(axis)
+// 		.selectAll('text')
+// 		.style('text-anchor', 'end')
+// 		.style('font-size', '16px')
+// 		.attr('dx', '-2em')
+// 		.attr('dy', '1em')
+// 		.attr('transform', 'rotate(-65)');
+// 	drawTimelineEvents(rootElement, windowSize, timelineEvents);
+// };
+
 import { select } from 'd3-selection';
 import { axisBottom } from 'd3-axis';
 import { scaleLinear } from 'd3-scale';
+import { drawTimelineEvents } from './drawTimelineEvents';
 
 export const drawTimeline = (rootElement, windowSize, timelineEvents) => {
 	const { width, height } = windowSize;
@@ -16,7 +83,7 @@ export const drawTimeline = (rootElement, windowSize, timelineEvents) => {
 	const axis = axisBottom(scale)
 		.ticks()
 		.tickFormat((d) => {
-			console.log('d', d);
+			// console.log("d", d);
 			const date = new Date(d);
 			return `${date.toLocaleDateString()}`;
 		});
@@ -34,4 +101,5 @@ export const drawTimeline = (rootElement, windowSize, timelineEvents) => {
 		.attr('dx', '-2em')
 		.attr('dy', '1em')
 		.attr('transform', 'rotate(-65)');
+	// drawTimelineEvents(rootElement, windowSize, timelineEvents);
 };
